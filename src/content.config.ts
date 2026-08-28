6fc243fe4036c6b095a1e8a53f59c51a247d92ca
Matt <github@mdbell.me>
2026-08-28 11:04:02 -0400

    placeholder article about awtea

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,10 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
@@ -14,6 +14,7 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			draft: z.boolean().optional(),
 		}),
 });
 
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,10 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection("blog"))
+		.filter((res) => import.meta.env.PROD ? !res.data.draft : true);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
